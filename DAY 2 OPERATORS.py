08eedaf3ead122d98e6f2053ecc99a8617f088b0
PRATHYUSHA GALLA <65410374+Prathyusha10@users.noreply.github.com>
2021-02-12 12:28:57 +0530

    Add files via upload

---
+++ DAY 2 OPERATORS.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip_value= meal_cost*tip_percent/100
+    tax_value= meal_cost*tax_percent/100
+    total_cost = meal_cost+tip_value+tax_value
+    print(round(total_cost))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
+    
+
+
+# In[ ]:
+
+
+
+
