af54f616964f9718889b504ec5d181f793b3ab7b
PRATHYUSHA GALLA <65410374+Prathyusha10@users.noreply.github.com>
2021-02-13 22:26:16 +0530

    Add files via upload

---
+++ Day 5 Loops.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        print('{0} x {1} = {2}'.format(n,i,n*i))
+
+
+# In[ ]:
+
+
+
+
