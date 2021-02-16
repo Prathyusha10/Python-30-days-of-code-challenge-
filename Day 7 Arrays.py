9535f2e21f3b8e86daf6e1fceb8a94768f5e2247
PRATHYUSHA GALLA <65410374+Prathyusha10@users.noreply.github.com>
2021-02-16 20:49:54 +0530

    Add files via upload

---
+++ Day 7 Arrays.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    arr = arr[::-1]
+    for i in arr:
+        print(i,end =' ')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
