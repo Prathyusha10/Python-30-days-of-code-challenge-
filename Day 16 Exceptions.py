484bb686c8ddb63145b3d51e3ad983b4fb77c91a
PRATHYUSHA GALLA <65410374+Prathyusha10@users.noreply.github.com>
2021-02-25 21:39:16 +0530

    Add files via upload

---
+++ Day 16 Exceptions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+
+
+S = input().strip()
+try:
+    print(int(S))
+except ValueError:
+    print('Bad String')
+
+
+# In[ ]:
+
+
+
+
