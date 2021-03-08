dcfe75730da84f2981f3562a68eb0ba340c4206c
PRATHYUSHA GALLA <65410374+Prathyusha10@users.noreply.github.com>
2021-03-08 20:18:26 +0530

    Add files via upload

---
+++ Day 28 RegEx, Patterns, and Intro to Databases.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <set>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    regex e(".+@gmail\\.com$");
+    multiset<string> db;
+    for(int a0 = 0; a0 < N; a0++)
+    {
+        string firstName;
+        string emailID;
+        cin >> firstName >> emailID;
+        if(regex_match(emailID,e))
+        {
+            db.insert(firstName);
+        }
+    }
+    // just a fancy and less robust way of iterating in 
+    // C++11 (and later)
+    for(auto& it : db) 
+    { 
+        cout << it << endl; 
+    }
+    return 0;
+}
+
+
+
